d8d1c22af39faa228557d42c64d0bb9049762e2b
uttampun <uttampun62@gmail.com>
2025-05-14 13:35:05 +0545

    JP-11 table implement jobs

--- frontend-job-pilot/src/pages/backend/jobs/Jobs.tsx
+++ frontend-job-pilot/src/pages/backend/jobs/Jobs.tsx
@@ -1,8 +1,50 @@
+import { Button } from "@/components/ui/button";
+import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import useFetch from "@/hooks/api/useFetch";
+import React from "react";
+
 export default function Jobs() {
-    
+
+    const { data: data } = useFetch('/api/v1/jobs');
+
     return (
-        <div>
-            <h1>Jobs</h1>
-        </div>
+        <React.Fragment>
+            <Table>
+                <TableCaption>A list of your recent invoices.</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[100px]">Invoice</TableHead>
+                        <TableHead>Status</TableHead>
+                        <TableHead>Method</TableHead>
+                        <TableHead className="text-right">Amount</TableHead>
+                        <TableHead>Action</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {invoices.map((invoice) => (
+                        <TableRow key={invoice.invoice}>
+                            <TableCell className="font-medium">{invoice.invoice}</TableCell>
+                            <TableCell>{invoice.paymentStatus}</TableCell>
+                            <TableCell>{invoice.paymentMethod}</TableCell>
+                            <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+                            <TableCell>
+                                <Button type="button"
+                                 variant="outlined" color="primary"
+                                onClick={() => console.log("Delete")}
+                                 >
+                                    Delete
+                                </Button>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+                <TableFooter>
+                    <TableRow>
+                        <TableCell colSpan={3}>Total</TableCell>
+                        <TableCell className="text-right">$2,500.00</TableCell>
+                    </TableRow>
+                </TableFooter>
+            </Table>
+        </React.Fragment>
     );
 }
