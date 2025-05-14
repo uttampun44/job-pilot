59e1cfb9d0fb57a86ecc9630f82c2ec387b4eda8
uttampun <uttampun62@gmail.com>
2025-05-14 17:02:05 +0545

    JP-11 redux toolkit added

--- backend-job-pilot/Modules/Jobs/routes/api.php
+++ backend-job-pilot/Modules/Jobs/routes/api.php
@@ -6,3 +6,5 @@ use Modules\Jobs\Http\Controllers\JobsController;
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
     Route::apiResource('jobs', JobsController::class)->names('jobs');
 });
+
+ Route::apiResource('jobs', JobsController::class)->names('jobs');
